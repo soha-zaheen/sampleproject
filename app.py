9c800792efaa0a73e23b8bf3473d3a21a22e2e0f
soha-zaheen <zaheensoha@gmail.com>
2025-08-02 12:21:42 +0530

    third update

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask,render_template
 app=Flask(__name__)
 @app.route("/")
 def index():
-    return "Welcome to mrecw"
+    return render_template("index.html")
 @app.route("/home")
 def home():
     return "home page"
