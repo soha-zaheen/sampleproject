a392668a5e179d472fd34d2966e609085c76186d
soha-zaheen <zaheensoha@gmail.com>
2025-08-02 11:50:22 +0530

    second update

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     return "Welcome to mrecw"
+@app.route("/home")
+def home():
+    return "home page"
 if __name__=="__main__":
     app.run(host='0.0.0.0',port=10000)
